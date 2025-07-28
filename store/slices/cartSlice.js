4b3be811feae7479a3c879673d5da17c44e65ebf
Moksh-Git <patilmokshadg@gmail.com>
2025-07-29 00:59:51 +0530

    implemented all operations using produce method provided by immer

--- store/slices/cartSlice.js
+++ store/slices/cartSlice.js
@@ -39,40 +39,30 @@ export function removeCartItemQuantity(productId, quantity) {
 // Reducers
 export default function cartReducer(originalState = [], action) {
   return produce(originalState, (state) => {
-      const existingItemIndex = state.findIndex(
-          (item) => item.productId === action.payload.productId
-        );
+    const existingItemIndex = state.findIndex(
+      (item) => item.productId === action.payload.productId
+    );
     switch (action.type) {
       case CART_ADD_ITEMS:
         if (existingItemIndex !== -1) {
-          state[existingItemIndex].quantity += 1
-          return state
+          state[existingItemIndex].quantity += 1;
+          break;
         }
-        state.push({ ...action.payload, quantity: 1 })
-        return state
-
+        state.push({ ...action.payload, quantity: 1 });
+        break;
       case CART_REMOVE_ITEMS:
-        state.splice(existingItemIndex,1)
-        return state
+        state.splice(existingItemIndex, 1);
+        break;
 
       case CART_ITEMS_INCREASE_QUANTITY:
-        return state.map((cartItem) => {
-          if (cartItem.productId === action.payload.productId)
-            return { ...cartItem, quantity: cartItem.quantity + 1 };
-          return cartItem;
-        });
+        state[existingItemIndex].quantity += 1;
+        break;
 
       case CART_ITEMS_DECRESE_QUANTITY:
-        return state
-          .map((item) => {
-            if (item.productId === action.payload.productId)
-              return { ...item, quantity: item.quantity - 1 };
-            return item;
-          })
-          .filter((item) => item.quantity > 0);
-
-      default:
-        return state;
+        state[existingItemIndex].quantity -= 1;
+        if (state[existingItemIndex].quantity === 0)
+          state.splice(existingItemIndex, 1);
     }
+    return state;
   });
 }
